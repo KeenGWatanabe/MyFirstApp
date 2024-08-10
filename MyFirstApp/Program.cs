cebfca89d2301518a4234bcfa47cc8dd81c5b36c
60936 <studio34pl@gmail.com>
2024-08-10 20:24:22 +0800

    r

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    public static void Main()
+    {
+        float vat = 19f;
+        float productA = 3.99f;
+        float productB = 11.99f;
+        float productC = 5.75f;
+
+        float summed = productA + productB + productC;
+        float totalTax = (summed /100  ) * vat;
+        Console.WriteLine("summed " + summed + " and totalTax " + totalTax);
+
+        summed += totalTax;
+        Console.WriteLine("Total price with vat is " + summed);
+    }
+}
